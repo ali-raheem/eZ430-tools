dbf4b70c1a0909e5a54468f670d32b229eddbc16
Ali Raheem <ali.raheem@gmail.com>
2010-10-10 11:16:12 +0100

    Adding verbosity switch.

--- ppt.py
+++ ppt.py
@@ -2,6 +2,7 @@
 import os
 import eZ430
 
+verbose = 1
 watch = eZ430.sensors()
 print "Opening eZ430 on",watch.dev
 if(os.system("xdotool --version")!=0):
@@ -11,14 +12,14 @@ while 1:
 	data = watch.read(7)
 	button = int(ord(data[6]))
 	if(button == 18):
-		print "Right pressed."
+		if(verbose): print "Right pressed."
 		os.system("xdotool click 3")
 		watch.debounce()
 	elif(button == 50):
-		print "Left pressed."
+		if verbose: print "Left pressed."
 		os.system("xdotool click 1")
 		watch.debounce()
 	elif(button == 34):
-		print "Home pressed."
+		if verbose: print "Home pressed."
 		os.system("xdotool key Home")
 		watch.debounce()
