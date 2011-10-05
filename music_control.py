6c7fe5dc33988c16a14a6915514aba0429841737
Christoffer Loeffler <chris@loefflerhaus.de>
2011-10-06 01:34:55 +0200

    smooth volume (strong tilt faster change) holding head gesture disables volume change ;) pause gesture (up) unpause and next title gesture (down)

--- music_control.py
+++ music_control.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import eZ430, dbus
+import eZ430, dbus, time, math
 #Rhythmbox dbus
 session_bus = dbus.SessionBus()
 proxy_obj = session_bus.get_object('org.gnome.Rhythmbox', '/org/gnome/Rhythmbox/Player')
@@ -9,12 +9,20 @@ watch = eZ430.watch()
 
 #Variables
 #link=0
-pd=0
+#pd=0
 r=0
-s=0
-p=0
+l=0
+down=0
+up=0
 playing=0
-#val=[0,0,0,0]
+paused=0
+
+downmillies=int(round(time.time()*1000))
+upmillies=int(round(time.time()*1000))
+
+x=0
+y=0
+z=0
 
 #Gestures
 def raised():
@@ -26,61 +34,67 @@ def raised():
 def pronate():
 	print "Gesture detected!"
 	print "Name:\tPronate"
-	print "Bind:\tVolume +20%\n"
-	player.setVolumeRelative(0.2)
+	print "Bind:\tVolume +1%\n"
+	player.setVolumeRelative(0.01)
 def supanate():
 	print "Gesture detected!"
 	print "Name:\tSupanate"
-	print "Bind:\tVolume -20%\n"
-	player.setVolumeRelative(-0.2)
+	print "Bind:\tVolume -1%\n"
+	player.setVolumeRelative(-0.01)
 def swing_down():
 	print "Gesture detected!"
 	print "Name:\tSwing down"
 	print "Bind:\tNext track\n"
 	player.next()
-def clear():
-	pd=0
-	s=0
-	p=0
-	r=0
+#def clear():
+#	down=0
+#	l=0
+#	r=0
 while 1:
 	data = watch.read()
    	acc={'x':ord(data[0]), 'y':ord(data[1]), 'z':ord(data[2])}
 	if acc['x']+acc['y']+acc['z']!=0:
-#		print "x: %s\ty:%s\tz:%s\tpd:%s"%(acc['x'],acc['y'],acc['z'],pd)
-		x=acc['x']
-		y=acc['y']
-		z=acc['z']
-	if (y>128) & (x<128):
-		pd+=1
-		r+=1
-	else:
-		pd-=1
-	if (y<128):
-		r=0
-	if pd>10:
-		pd=10
-	if pd<0:
-		pd=0
-	if p<0:
-		p=0
-	if s<0:
-		s=0
-	if (y<128) & (pd>7):
-		pd=0
-		swing_down()
-	if (r>500):
-		r=0
-		raised()
-	if (x>128)&(y>128)&(z>128):
-		p+=1
-		s=0
-	else:
-		p=0
-		s+=1
-	if p>500:
-		p=0
+		x=(acc['x'] + x) /2
+		y=(acc['y'] + y) /2
+		z=(acc['z'] + z) /2
+		print "x: %s\ty:%s\tz:%s"%(acc['x'],acc['y'],acc['z'])
+	if (y>25) & (y<100) & (z>35):
+		down+=1
+	if (y<220) & (y>190) & (z>240) & (z<250):
+		up+=1
+	# volume, but only if hand not holding the head ;)
+	if (y>230) or (y<30):
+		if (x>20) & (x<=35):
+			r+=1;
+		elif (x>35) & (x<55):
+			r+=8 #strong tilt
+		elif (x<225) & (x>=205):
+			l+=1
+		elif (x<205) & (x>190):
+			l+=8 #strong tilt
+		else:
+			r=0
+			l=0
+	if (r>20):
 		pronate()
-	if s>500:
-		s=0
-#		supanate()
+		r=0
+	if (l>20):
+		supanate()
+		l=0
+	#down gesture unpaus OR next title
+	if (down>5) & (downmillies-int(round(time.time()*1000))<-2000):
+		downmillies=int(round(time.time()*1000))
+		down=0
+		up=0
+		if (paused==1):
+				paused=0
+				raised()
+		else:
+			swing_down()
+	#raise hand to pause, down gesture to unpause
+	if (up>5) & (paused==0) & (upmillies-int(round(time.time()*1000))<-2000):
+		paused=1
+		upmillies=int(round(time.time()*1000))
+		up=0
+		down=0
+		raised()
